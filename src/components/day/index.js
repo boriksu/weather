5fe5bd232720da5ee3c4c14e58c8e234f3c6287a
OUT-Borisova <out-borisova@cab-wsm-0010138.sigma.sbrf.ru>
2021-01-21 01:33:41 +0300

    add some styles

---
+++ src/components/day/index.js
@@ -0,0 +1,9 @@
+// <div className="forecast">
+//   <div className="day sunny selected"><p>среда</p><span>29</span></div>
+//   <div className="day sunny"><p>четверг</p><span>21</span></div>
+//   <div className="day sunny"><p>пятница</p><span>25</span></div>
+//   <div className="day cloudy"><p>суббота</p><span>20</span></div>
+//   <div className="day sunny"><p>воскресенье</p><span>27</span></div>
+//   <div className="day rainy"><p>понедельник</p><span>22</span></div>
+//   <div className="day rainy"><p>вторник</p><span>26</span></div>
+// </div>
--- src/components/dayList/index.js
+++ src/components/dayList/index.js
@@ -1,25 +1,41 @@
 import React from 'react'
 import {connect} from "react-redux";
 import {bindActionCreators} from "redux";
+// import {moment} from "moment"
+// import Moment from 'moment'
+import "./../../styles2/index.scss"
+const date = (day) => {
+  // let time = moment().format('dddd');
+  var moment = require('moment');
+  moment.locale('ru')
+  return moment(day).format('dddd');
+
+}
 
 export const DayList = ({dayList}) => {
         return (
-            <ul>
+          <div className="forecast">
                 <p>List</p>
                 {
                     dayList != null && dayList.map((day) => {
                         // console.log("###DAY    ", day)
                         return (
-                            <li key={day.day}>
 
-                                <p>{day.type}</p>
-                                {/*<p>{day.sun}</p>*/}
+                          <div className="day sunny">
+                            <p>{date(day.day)}</p>
+                            <span>{day.temperature}</span>
+                          </div>
 
-                            </li>
+                            // <li key={day.day}>
+                            //
+                            //     <p>{data(day.day)}</p>
+                            //     {/*<p>{day.sun}</p>*/}
+                            //
+                            // </li>
                         )
                     })
                 }
-            </ul>)
+            </div>)
 }
 
 function mapStateToProps(state) {
